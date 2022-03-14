53c5b74fe641270ad43fa88eebe8f01655be2ef0
Max Horstmann <max.horstmann@gmail.com>
2022-03-13 21:30:30 -0400

    2nd example

--- cmd/linq101/main.go
+++ cmd/linq101/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 )
 
-func where[T comparable](input []T, pred func(T) bool) []T {
+type collection[T comparable] []T
+
+func (input collection[T]) where(pred func(T) bool) []T {
 	result := []T{}
 	for _, j := range input {
 		if pred(j) {
@@ -14,9 +16,30 @@ func where[T comparable](input []T, pred func(T) bool) []T {
 	return result
 }
 
-func main() {
-	numbers := []int{5, 4, 1, 3, 9, 8, 6, 7, 2, 0}
-	lowNums := where(numbers, func(i int) bool { return i < 5 })
+func linq1() {
+	numbers := collection[int]{5, 4, 1, 3, 9, 8, 6, 7, 2, 0}
+	lowNums := numbers.where(func(i int) bool { return i < 5 })
 	fmt.Println("Numbers < 5:")
 	fmt.Println(lowNums)
+}
+
+type product struct {
+	ProductName  string
+	UnitsInStock int
+}
+
+func linq2() {
+	products := collection[product]{
+		{ProductName: "milk", UnitsInStock: 0},
+		{ProductName: "water", UnitsInStock: 4},
+		{ProductName: "beer", UnitsInStock: 0}}
+	soldOutProducts := products.where(func(p product) bool { return p.UnitsInStock == 0 })
+	fmt.Println("Sold out products:")
+	for _, p := range soldOutProducts {
+		fmt.Printf("%s is sold out!\n", p.ProductName)
+	}
+}
+
+func main() {
+	linq2()
 }
